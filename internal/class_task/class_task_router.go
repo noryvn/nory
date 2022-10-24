25a6e13b8baa1814f96f141a23aa050fa3c4ea11
falentio <riv.kevinfalent.io@gmail.com>
2022-10-25 00:44:35 +0700

    feat: task endpoint

---
+++ internal/class_task/class_task_router.go
@@ -0,0 +1,32 @@
+package classtask
+
+import (
+	"nory/common/auth"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type classTaskRouter struct{
+	cts ClassTaskService
+}
+
+func Route(classTaskService ClassTaskService) func (router fiber.Router) {
+	ctr := classTaskRouter{classTaskService}
+	return func (router fiber.Router) {
+		router.Delete("/:taskId", ctr.DeleteTask)
+	}
+}
+
+func (ctr classTaskRouter) DeleteTask(c *fiber.Ctx) error {
+	user, err := auth.GetUser(c)
+	if err != nil {
+		return err
+	}
+
+	taskId := c.Params("taskId")
+	res, err := ctr.cts.DeleteTask(c.Context(), taskId, user.UserId)
+	if err != nil {
+		return err
+	}
+	return res.Respond(c)
+}
