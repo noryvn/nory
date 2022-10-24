590aea715e701dd6454987e4099704c385a3f30d
falentio <riv.kevinfalent.io@gmail.com>
2022-10-24 19:05:17 +0700

    feat: user service testing

---
+++ internal/user/user_service_test.go
@@ -0,0 +1,73 @@
+package user_test
+
+import (
+	"context"
+	"testing"
+
+	"nory/domain"
+	"nory/internal/class"
+	. "nory/internal/user"
+
+	"github.com/google/uuid"
+	"github.com/rs/xid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestUserService(t *testing.T) {
+	userRepository := NewUserRepositoryMem()
+	classRepository := class.NewClassRepositoryMem()
+
+	us := UserService{
+		UserRepository:  userRepository,
+		ClassRepository: classRepository,
+	}
+
+	t.Run("GetUserProfile", func(t *testing.T) {
+		t.Parallel()
+		user := &domain.User{
+			UserId: uuid.NewString(),
+			Name: "Abelia",
+			Username: xid.New().String(),
+			Email: xid.New().String(),
+		}
+		err := us.UserRepository.CreateUser(context.Background(), user)
+		assert.Nil(t, err)
+
+		res, err := us.GetUserProfile(context.Background(), user)
+		assert.Nil(t, err)
+		assert.Equal(t, 200, res.Code)
+		assert.Equal(t, user.Name, res.Data.User.Name)
+		assert.Equal(t, user.UserId, res.Data.User.UserId)
+	})
+
+	t.Run("GetUserClasses", func(t *testing.T) {
+		t.Parallel()
+		user := &domain.User{
+			UserId: uuid.NewString(),
+			Name: "Abelia",
+			Username: xid.New().String(),
+			Email: xid.New().String(),
+		}
+		err := us.UserRepository.CreateUser(context.Background(), user)
+		assert.Nil(t, err)
+
+		var classes []*domain.Class
+		for i := 0; i < 5; i++ {
+			class := &domain.Class{
+				OwnerId:     user.UserId,
+				Name:        "foo",
+			}
+			classes = append(classes, class)
+			err := us.ClassRepository.CreateClass(context.Background(), class)
+			assert.Nil(t, err)
+		}
+
+		res, err := us.GetUserClasses(context.Background(), user)
+		assert.Nil(t, err)
+		assert.Equal(t, len(classes), len(res.Data))
+		for _, class := range res.Data {
+			assert.Equal(t, "foo", class.Name)
+			assert.Equal(t, user.UserId, class.OwnerId)
+		}
+	})
+}
