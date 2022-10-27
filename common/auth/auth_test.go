ffb04e22f934088c75613e8f409d7b53c4dc4eb7
falentio <riv.kevinfalent.io@gmail.com>
2022-10-27 20:16:38 +0700

    chore: more auth testing middleware

--- common/auth/auth_test.go
+++ common/auth/auth_test.go
@@ -3,13 +3,16 @@ package auth_test
 import (
 	"context"
 	"fmt"
+	"net/http/httptest"
 	"os"
 	"testing"
 
+	"github.com/gofiber/fiber/v2"
 	"github.com/nedpals/supabase-go"
 	"github.com/stretchr/testify/assert"
 
 	. "nory/common/auth"
+	"nory/common/response"
 	"nory/internal/user"
 )
 
@@ -41,4 +44,33 @@ func TestAuthMiddleware(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, supaUser.User.ID, user.UserId, "unknown login data received")
 	assert.Equal(t, supaUser.User.Email, user.Email, "unknown login data received")
+
+	app := fiber.New(fiber.Config{
+		Immutable: true,
+		ErrorHandler: func(c *fiber.Ctx, err error) error {
+			if res, ok := err.(*response.ResponseError); ok {
+				return res.Respond(c)
+			}
+			return fiber.DefaultErrorHandler(c, err)
+		},
+	})
+	app.Use(a.Middleware)
+	app.Get("/", func(c *fiber.Ctx) error {
+		user, err := GetUser(c)
+		if err != nil {
+			return err
+		}
+		return c.JSON(user)
+	})
+
+	req := httptest.NewRequest("GET", "/", nil)
+	req.Header.Set(fiber.HeaderAuthorization, bearer)
+	resp, err := app.Test(req)
+	assert.Nil(t, err)
+	assert.Equal(t, 200, resp.StatusCode)
+
+	req = httptest.NewRequest("GET", "/", nil)
+	resp, err = app.Test(req)
+	assert.Nil(t, err)
+	assert.Equal(t, 401, resp.StatusCode)
 }
