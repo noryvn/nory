363a4fc623e9b1a9f1f85f3b86e8f050ba439120
falentio <riv.kevinfalent.io@gmail.com>
2022-10-27 22:51:57 +0700

    feat: class member domain

--- domain/class_member.go
+++ domain/class_member.go
@@ -1,1 +1,17 @@
 package domain
+
+import "context"
+
+type ClassMember struct {
+	ClassId string
+	UserId string
+
+	Level string
+}
+
+type ClassMemberRepository interface {
+	ListMembers(ctx context.Context, classId string) ([]*ClassMember, error)
+	IsMember(ctx context.Context, classId, memberId string) (bool, error)
+	CreateMember(ctx context.Context, member *ClassMember) error
+	DeleteMember(ctx context.Context, classId, memberId string) error
+}
