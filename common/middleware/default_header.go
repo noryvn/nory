9374786d630226438adae2639b9bea2ea1f2978a
falentio <riv.kevinfalent.io@gmail.com>
2022-11-10 17:44:28 +0700

    feat(server): default header

---
+++ common/middleware/default_header.go
@@ -0,0 +1,8 @@
+package middleware
+
+import "github.com/gofiber/fiber/v2"
+
+func DefaultHeader(c *fiber.Ctx) (error) {
+	c.Set("cache-control", "private, max-age=0, no-store")
+	return c.Next()
+}
