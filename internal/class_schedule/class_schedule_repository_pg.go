07129e676f23e0c7cf9c1f22746335a36846be5f
falentio <riv.kevinfalent.io@gmail.com>
2022-11-10 17:56:26 +0700

    feat(internal/classschedule):

--- internal/class_schedule/class_schedule_repository_pg.go
+++ internal/class_schedule/class_schedule_repository_pg.go
@@ -1,1 +1,38 @@
 package classschedule
+
+import (
+	"context"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+
+	"nory/domain"
+)
+
+type ClassScheduleRepositoryPg struct {
+	pool *pgxpool.Pool
+}
+
+func NewClassScheduleRepositoryPg(pool *pgxpool.Pool) *ClassScheduleRepositoryPg {
+	return &ClassScheduleRepositoryPg{pool}
+}
+
+func (csrp *ClassScheduleRepositoryPg) CreateSchedule(ctx context.Context, schedule *domain.ClassSchedule) error {
+	return nil
+}
+
+func (csrp *ClassScheduleRepositoryPg) GetSchedule(ctx context.Context, scheduleId string) (*domain.ClassSchedule, error) {
+	return nil, nil
+}
+
+func (csrp *ClassScheduleRepositoryPg) GetSchedules(ctx context.Context, classId string) ([]*domain.ClassSchedule, error) {
+	return nil, nil
+}
+
+func (csrp *ClassScheduleRepositoryPg) DeleteSchedule(ctx context.Context, scheduleId string) error {
+	return nil
+}
+
+func (csrp *ClassScheduleRepositoryPg) ClearSchedules(ctx context.Context, classId string, day int8) error {
+	return nil
+}
+
